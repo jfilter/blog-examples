7e6b913d8fab5fbba9f7d6fdf4b988817ffe0463
Johannes Filter <hi@jfilter.de>
2017-04-14 22:41:24 +0300

    comment

--- r/help-seeking-on-facebook/plots.R
+++ r/help-seeking-on-facebook/plots.R
@@ -2,10 +2,11 @@ library(ggplot2)
 library(dplyr)
 library(scales)
 
+# change default color palette
 cbPalette <- c("#D55E00", "#0072B2", "#E69F00" ,"#999999", "#56B4E9", "#009E73", "#F0E442", "#CC79A7")
 
 # work on a copy of the data
-draw_graph <- function(df, prefix_column, x_title, new_levels, indep_col_name, question, titles) {
+draw_chart <- function(df, prefix_column, x_title, new_levels, indep_col_name, question, titles) {
   for (i in 1:length(titles)) {
     col_name <- paste(prefix_column, i, sep="")
 
@@ -16,27 +17,27 @@ draw_graph <- function(df, prefix_column, x_title, new_levels, indep_col_name, q
 
     levels(cur_df$dep_col) <- new_levels
 
-    # get relative data
+    # calculate the percentages in regard to the independent variable
     cur_df <- cur_df %>%
-      group_by(indep_col, dep_col) %>%
+      group_by(indep_col, dep_col) %>% # NB: the order of the grouping
       summarise(count=n()) %>%
       mutate(perc=count/sum(count))
 
     diagram_title <- paste(question, titles[i], "?", sep="")
 
-    ggplot(data=cur_df, aes(x=dep_col, y=perc, fill=indep_col)) +
-      geom_bar(width=0.7, position=position_dodge(width=0.75), stat="identity") +
-      scale_y_continuous(labels=scales::percent, limits=c(0, 0.5)) +
+    # draw the diagram
+    ggplot(data=cur_df, aes(x=dep_col, y=perc, fill=indep_col)) + # assign how to represent columns
+      geom_bar(width=0.7, position=position_dodge(width=0.75), stat="identity") + # adjust width &
+      scale_y_continuous(labels=scales::percent, limits=c(0, 0.5)) + # alter y axis
       labs(y = "Percentage", x=x_title, title=diagram_title, fill=indep_col_name) +
-      theme_bw() +
-      theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(), plot.title = element_text(hjust = 0.5)) +
-      scale_fill_manual(values=cbPalette)
+      theme_bw() + # change the general theme
+      theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(), plot.title = element_text(hjust = 0.5)) + # remove vertical lines and center title
+      scale_fill_manual(values=cbPalette) # changes colors
 
-    # path <- "p"
+    # save the image and create folders if needed
     dir_path <- paste(getwd(), "/graphs/", indep_col_name, sep="")
-    dir.create(dir_path, recursive=TRUE, showWarnings=FALSE)
-
-    file_name <- paste(as.character(x_title), titles[i], ".png", sep="")
+    dir.create(dir_path, recursive=TRUE, showWarnings=FALSE) # warning if folder already existent
+    file_name <- paste(x_title, titles[i], ".png", sep="")
     ggsave(file_name, last_plot(), "png", path=dir_path)
   }
 }
@@ -53,15 +54,15 @@ e_levels <- c("","1 (ineffective)", "2", "3", "4", "5 (effective)");
 f_question <- "How often do you see people asking for help on Facebook\nregarding "
 e_question <- "How effective are help-seeking posts on Facebook\nregarding "
 
-# filter out unspecified gender because of missing results
+# filter out unspecified gender because there weren't enough responses
 data = subset(data, gender == "male" | gender == "female")
 
-draw_graph(data, "f_", "Frequency", f_levels, "gender", f_question, titles)
-draw_graph(data, "e_", "Effectivity",  e_levels, "gender", e_question, titles)
+draw_chart(data, "f_", "Frequency", f_levels, "gender", f_question, titles)
+draw_chart(data, "e_", "Effectivity",  e_levels, "gender", e_question, titles)
 
+# merge age buckets into one because there weren't enough responses
 i2 <- levels(data$age) %in% c("35 - 44", "45 - 54", "55 - 64", "65 +")
 levels(data$age)[i2] <- "35 +"
 
-draw_graph(data, "f_", "Frequency", f_levels, "age", f_question, titles)
-draw_graph(data, "e_", "Effectivity",  e_levels, "age", e_question, titles)
-
+draw_chart(data, "f_", "Frequency", f_levels, "age", f_question, titles)
+draw_chart(data, "e_", "Effectivity",  e_levels, "age", e_question, titles)
