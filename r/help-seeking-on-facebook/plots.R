7fe5486240ea5a311bf03831777f847f9b2ba516
Johannes Filter <hi@jfilter.de>
2017-04-14 21:28:04 +0300

    improve code

--- r/help-seeking-on-facebook/plots.R
+++ r/help-seeking-on-facebook/plots.R
@@ -2,37 +2,41 @@ library(ggplot2)
 library(dplyr)
 library(scales)
 
+cbPalette <- c("#D55E00", "#0072B2", "#E69F00" ,"#999999", "#56B4E9", "#009E73", "#F0E442", "#CC79A7")
+
 # work on a copy of the data
-do_graph <- function(df, prefix_column, prefix_title, own_levels, indep_col_name) {
-  for (i in 1:9) {
+draw_graph <- function(df, prefix_column, x_title, new_levels, indep_col_name, question, titles) {
+  for (i in 1:length(titles)) {
     col_name <- paste(prefix_column, i, sep="")
-    col_title <- paste(prefix_title, titles[i], sep=" ")
 
     # selecting only two wanted columns and renaming them
     cur_df <- df[,c(indep_col_name ,col_name)]
     colnames(cur_df) <- c("indep_col" , "dep_col")
-    cur_df <- subset(cur_df, dep_col != "" | indep_col != "")
+    cur_df <- subset(cur_df, dep_col != "" & indep_col != "")
 
-    levels(cur_df$dep_col) <- own_levels
+    levels(cur_df$dep_col) <- new_levels
 
-    # group by
+    # get relative data
     cur_df <- cur_df %>%
       group_by(indep_col, dep_col) %>%
       summarise(count=n()) %>%
       mutate(perc=count/sum(count))
 
+    diagram_title <- paste(question, titles[i], "?", sep="")
+
     ggplot(data=cur_df, aes(x=dep_col, y=perc, fill=indep_col)) +
-      geom_bar(width=0.7, position=position_dodge(), stat="identity") +
+      geom_bar(width=0.7, position=position_dodge(width=0.75), stat="identity") +
       scale_y_continuous(labels=scales::percent, limits=c(0, 0.5)) +
-      labs(y = "Percentage", x=prefix_title, title=col_title, fill=indep_col_name) +
+      labs(y = "Percentage", x=x_title, title=diagram_title, fill=indep_col_name) +
       theme_bw() +
-      theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())
+      theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank(), plot.title = element_text(hjust = 0.5)) +
+      scale_fill_manual(values=cbPalette)
 
     # path <- "p"
     dir_path <- paste(getwd(), "/graphs/", indep_col_name, sep="")
     dir.create(dir_path, recursive=TRUE, showWarnings=FALSE)
 
-    file_name <- paste(as.character(col_title), ".png", sep="")
+    file_name <- paste(as.character(x_title), titles[i], ".png", sep="")
     ggsave(file_name, last_plot(), "png", path=dir_path)
   }
 }
@@ -46,12 +50,18 @@ titles <- c("Technology", "Entertainment", "Home and Family", "Work and Study", 
 f_levels <- c("never", "rarely", "sometimes", "often", "very often");
 e_levels <- c("","1 (ineffective)", "2", "3", "4", "5 (effective)");
 
+f_question <- "How often do you see people asking for help on Facebook\nregarding "
+e_question <- "How effective are help-seeking posts on Facebook\nregarding "
+
 # filter out unspecified gender because of missing results
 data = subset(data, gender == "male" | gender == "female")
 
-do_graph(data, "f_", "Frequency", f_levels, "gender")
-do_graph(data, "e_", "Effective",  e_levels, "gender")
+draw_graph(data, "f_", "Frequency", f_levels, "gender", f_question, titles)
+draw_graph(data, "e_", "Effectivity",  e_levels, "gender", e_question, titles)
 
-i2 <- levels(df$age) %in% c("35 - 44", "45 - 54", "55 - 64", "65 +")
-levels(df$age)[i2] <- "35 +"
+i2 <- levels(data$age) %in% c("35 - 44", "45 - 54", "55 - 64", "65 +")
+levels(data$age)[i2] <- "35 +"
+
+draw_graph(data, "f_", "Frequency", f_levels, "age", f_question, titles)
+draw_graph(data, "e_", "Effectivity",  e_levels, "age", e_question, titles)
 
