9740962c5a798ccdea0b4744b5647ab436b649da
Johannes Filter <hi@jfilter.de>
2017-04-14 18:16:55 +0300

    refactor code & style

--- r/help-seeking-on-facebook/plots.R
+++ r/help-seeking-on-facebook/plots.R
@@ -2,41 +2,56 @@ library(ggplot2)
 library(dplyr)
 library(scales)
 
-do_graph <- function(prefix, prefix_title, own_levels) {
+# work on a copy of the data
+do_graph <- function(df, prefix_column, prefix_title, own_levels, indep_col_name) {
   for (i in 1:9) {
-    col_name <- paste(prefix, i, sep="")
+    col_name <- paste(prefix_column, i, sep="")
     col_title <- paste(prefix_title, titles[i], sep=" ")
 
-    d2 <- d1[,c("age",col_name)]
-    colnames(d2) <- c("age", "xx")
-    d2 <- subset(d2, xx != "")
-    d1 <- subset(d1, gender == "female" | gender == "male")
+    # selecting only two wanted columns and renaming them
+    cur_df <- df[,c(indep_col_name ,col_name)]
+    colnames(cur_df) <- c("indep_col" , "dep_col")
+    cur_df <- subset(cur_df, dep_col != "" | indep_col != "")
 
-    i2 <- levels(d2$age) %in% c("35 - 44", "45 - 54", "55 - 64", "65 +")
-    levels(d2$age)[i2] <- "35 +"
+    levels(cur_df$dep_col) <- own_levels
 
-    d2 <- d2 %>%
-      group_by(age, xx) %>%
+    # group by
+    cur_df <- cur_df %>%
+      group_by(indep_col, dep_col) %>%
       summarise(count=n()) %>%
       mutate(perc=count/sum(count))
 
-    levels(d2$xx) <- own_levels
-
-    ggplot(data=d2, aes(x=xx, y = perc, fill=age)) +
+    ggplot(data=cur_df, aes(x=dep_col, y=perc, fill=indep_col)) +
       geom_bar(width=0.7, position=position_dodge(), stat="identity") +
-      scale_y_continuous(labels = scales::percent, limits=c(0, 0.5)) +
-      labs(y = "Percentage", x=prefix_title, title = col_title)
+      scale_y_continuous(labels=scales::percent, limits=c(0, 0.5)) +
+      labs(y = "Percentage", x=prefix_title, title=col_title, fill=indep_col_name) +
+      theme_bw() +
+      theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())
 
-    path <- paste("/Users/filter/code/helpseeking/graphs_age/", col_title, ".png", sep="")
-    ggsave(path, last_plot(), "png")
+    # path <- "p"
+    dir_path <- paste(getwd(), "/graphs/", indep_col_name, sep="")
+    dir.create(dir_path, recursive=TRUE, showWarnings=FALSE)
+
+    file_name <- paste(as.character(col_title), ".png", sep="")
+    ggsave(file_name, last_plot(), "png", path=dir_path)
   }
 }
 
-d1 = read.csv("/Users/filter/code/helpseeking/data.csv", header=TRUE, sep=",")
+data = read.csv("./data.csv", header=TRUE, sep=",")
 
-colnames(d1) <- c("time","gender", "age", "f_1", "f_2", "f_3", "f_4", "f_5", "f_6", "f_7", "f_8", "f_9", "e_1", "e_2", "e_3", "e_4", "e_5", "e_6", "e_7", "e_8", "e_9")
+colnames(data) <- c("time","gender", "age", "f_1", "f_2", "f_3", "f_4", "f_5", "f_6", "f_7", "f_8", "f_9", "e_1", "e_2", "e_3", "e_4", "e_5", "e_6", "e_7", "e_8", "e_9")
 
 titles <- c("Technology", "Entertainment", "Home and Family", "Work and Study", "Places to Visit", "Restaurants", "Current Events", "Shopping", "Ethics and Philosophy")
 
-do_graph("f_", "Frequency", c("never", "rarely", "sometimes", "often", "very often"))
-do_graph("e_", "Effectivity",  c("","1 (ineffective)", "2", "3", "4", "5 (effective)"))
+f_levels <- c("never", "rarely", "sometimes", "often", "very often");
+e_levels <- c("","1 (ineffective)", "2", "3", "4", "5 (effective)");
+
+# filter out unspecified gender because of missing results
+data = subset(data, gender == "male" | gender == "female")
+
+do_graph(data, "f_", "Frequency", f_levels, "gender")
+do_graph(data, "e_", "Effective",  e_levels, "gender")
+
+i2 <- levels(df$age) %in% c("35 - 44", "45 - 54", "55 - 64", "65 +")
+levels(df$age)[i2] <- "35 +"
+
