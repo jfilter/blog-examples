56117f3d9b6c1e439feed4ad6e7cab1377d0cae7
Johannes Filter <hi@jfilter.de>
2017-04-15 18:17:56 +0300

    improve readability

--- r/help-seeking-on-facebook/plots.R
+++ r/help-seeking-on-facebook/plots.R
@@ -6,12 +6,12 @@ library(scales)
 cbPalette <- c("#D55E00", "#0072B2", "#E69F00" ,"#999999", "#56B4E9", "#009E73", "#F0E442", "#CC79A7")
 
 # work on a copy of the data
-draw_chart <- function(df, prefix_column, x_title, new_levels, indep_col_name, question, titles) {
-  for (i in 1:length(titles)) {
+draw_chart <- function(df, prefix_column, x_title, new_levels, indep_col_name, question, topics) {
+  for (i in 1:length(topics)) {
     col_name <- paste(prefix_column, i, sep="")
 
     # selecting only two wanted columns and renaming them
-    cur_df <- df[,c(indep_col_name ,col_name)]
+    cur_df <- df[, c(indep_col_name, col_name)]
     colnames(cur_df) <- c("indep_col" , "dep_col")
     cur_df <- subset(cur_df, dep_col != "" & indep_col != "")
 
@@ -23,7 +23,7 @@ draw_chart <- function(df, prefix_column, x_title, new_levels, indep_col_name, q
       summarise(count=n()) %>%
       mutate(perc=count/sum(count))
 
-    diagram_title <- paste(question, titles[i], "?", sep="")
+    diagram_title <- paste(question, topics[i], "?", sep="")
 
     # draw the diagram
     ggplot(data=cur_df, aes(x=dep_col, y=perc, fill=indep_col)) + # assign how to represent columns
@@ -36,8 +36,8 @@ draw_chart <- function(df, prefix_column, x_title, new_levels, indep_col_name, q
 
     # save the image and create folders if needed
     dir_path <- paste(getwd(), "/graphs/", indep_col_name, sep="")
-    dir.create(dir_path, recursive=TRUE, showWarnings=FALSE) # warning if folder already existent
-    file_name <- paste(x_title, titles[i], ".png", sep="")
+    dir.create(dir_path, recursive=TRUE, showWarnings=FALSE) # ignore warning if folder already existent
+    file_name <- paste(x_title, topics[i], ".png", sep="")
     ggsave(file_name, last_plot(), "png", path=dir_path)
   }
 }
@@ -46,7 +46,7 @@ data = read.csv("./data.csv", header=TRUE, sep=",")
 
 colnames(data) <- c("time","gender", "age", "f_1", "f_2", "f_3", "f_4", "f_5", "f_6", "f_7", "f_8", "f_9", "e_1", "e_2", "e_3", "e_4", "e_5", "e_6", "e_7", "e_8", "e_9")
 
-titles <- c("Technology", "Entertainment", "Home and Family", "Work and Study", "Places to Visit", "Restaurants", "Current Events", "Shopping", "Ethics and Philosophy")
+topics <- c("Technology", "Entertainment", "Home and Family", "Work and Study", "Places to Visit", "Restaurants", "Current Events", "Shopping", "Ethics and Philosophy")
 
 f_levels <- c("never", "rarely", "sometimes", "often", "very often");
 e_levels <- c("","1 (ineffective)", "2", "3", "4", "5 (effective)");
@@ -57,12 +57,12 @@ e_question <- "How effective are help-seeking posts on Facebook\nregarding "
 # filter out unspecified gender because there weren't enough responses
 data = subset(data, gender == "male" | gender == "female")
 
-draw_chart(data, "f_", "Frequency", f_levels, "gender", f_question, titles)
-draw_chart(data, "e_", "Effectivity",  e_levels, "gender", e_question, titles)
+draw_chart(data, "f_", "Frequency", f_levels, "gender", f_question, topics)
+draw_chart(data, "e_", "Effectivity",  e_levels, "gender", e_question, topics)
 
 # merge age buckets into one because there weren't enough responses
 i2 <- levels(data$age) %in% c("35 - 44", "45 - 54", "55 - 64", "65 +")
 levels(data$age)[i2] <- "35 +"
 
-draw_chart(data, "f_", "Frequency", f_levels, "age", f_question, titles)
-draw_chart(data, "e_", "Effectivity",  e_levels, "age", e_question, titles)
+draw_chart(data, "f_", "Frequency", f_levels, "age", f_question, topics)
+draw_chart(data, "e_", "Effectivity",  e_levels, "age", e_question, topics)
